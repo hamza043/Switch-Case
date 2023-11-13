78dba7eaf8844b485e8ca6d6b7684ae0e0361cca
hamza043 <muhamadhamza474@gmail.com>
2023-11-13 23:40:51 +0500

    Switch cases

---
+++ app.js
@@ -0,0 +1,28 @@
+let grade = "F";
+
+switch(grade){
+    case "A":
+        console.log("A Grade");
+        break;
+        case "B":
+            console.log("B Grade");
+            break;
+            case "C":
+                console.log("C Grade");
+                break;
+                default:
+                    console.log("Failed");
+}
+
+let num = 10;
+
+switch(true){
+    case (num > 5):
+    console.log("If Chal rha hai");
+    break;
+    case (num < 5):
+        console.log("Else Chal rha hai");
+        break;
+        default:
+        console.log("kuch bhi nahe chala");
+}
